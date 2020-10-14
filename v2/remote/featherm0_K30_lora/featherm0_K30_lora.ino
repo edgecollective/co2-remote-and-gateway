ce11a187fa367f468052e479ecc343efd9024387
Don Blair <donblair@gmail.com>
2020-10-14 16:48:51 -0400

    update

---
+++ v2/remote/featherm0_K30_lora/featherm0_K30_lora.ino
@@ -0,0 +1,152 @@
+/*
+  Basic Arduino example for K-Series sensor
+  Created by Jason Berger
+  Co2meter.com  
+*/
+
+
+#include <SPI.h>
+#include <Wire.h>
+#include <RH_RF95.h>
+#include <ArduinoJson.h>
+
+
+byte readCO2[] = {0xFE, 0X44, 0X00, 0X08, 0X02, 0X9F, 0X25};  //Command packet to read Co2 (see app note)
+byte response[] = {0,0,0,0,0,0,0};  //create an array to store the response
+
+//multiplier for value. default is 1. set to 3 for K-30 3% and 10 for K-33 ICB
+int valMultiplier = 1;
+
+
+// for feather m0  
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+    StaticJsonDocument<200> doc;
+
+int led = 13;
+
+void setup() 
+{
+
+pinMode(led, OUTPUT);
+
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+
+  // put your setup code here, to run once:
+  Serial.begin(9600);         //Opens the main serial port to communicate with the computer
+  Serial1.begin(9600);    //Opens the virtual serial port with a baud of 9600
+  
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+  
+}
+
+void loop() 
+{
+  
+  sendRequest(readCO2);
+  unsigned long valCO2 = getValue(response);
+  //Serial.print("Co2 ppm = ");
+  Serial.println(valCO2);
+
+  doc["ppm"]=valCO2;
+
+char radiopacket[40];
+  serializeJson(doc, radiopacket);
+  
+  //char radiopacket[20] = "Hello World #      ";
+  //itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  //radiopacket[19] = 0;
+  
+  Serial.println("Sending...");
+  delay(10);
+  rf95.send((uint8_t *)radiopacket, 40);
+
+  Serial.println("Waiting for packet to complete..."); 
+  delay(10);
+  rf95.waitPacketSent();
+Serial.println("packet sent.");
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
+  
+  delay(2000);
+
+  
+}
+
+void sendRequest(byte packet[])
+{
+  while(!Serial1.available())  //keep sending request until we start to get a response
+  {
+    Serial1.write(readCO2,7);
+    delay(50);
+  }
+  
+  int timeout=0;  //set a timeoute counter
+  while(Serial1.available() < 7 ) //Wait to get a 7 byte response
+  {
+    timeout++;  
+    if(timeout > 10)    //if it takes to long there was probably an error
+      {
+        while(Serial1.available())  //flush whatever we have
+          Serial1.read();
+          
+          break;                        //exit and try again
+      }
+      delay(50);
+  }
+  
+  for (int i=0; i < 7; i++)
+  {
+    response[i] = Serial1.read();
+  }  
+}
+
+unsigned long getValue(byte packet[])
+{
+    int high = packet[3];                        //high byte for value is 4th byte in packet in the packet
+    int low = packet[4];                         //low byte for value is 5th byte in the packet
+
+  
+    unsigned long val = high*256 + low;                //Combine high byte and low byte with this formula to get value
+    return val* valMultiplier;
+}
